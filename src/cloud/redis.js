67a10b795c47262b140464e1bd12c25a60d8533a
秋天 <oncwnuLV-PkG9kbDOn8dNjvnrKb8@git.weixin.qq.com>
2022-05-12 13:36:21 +0800

    更redis库  ball11

--- src/cloud/redis.js
+++ src/cloud/redis.js
@@ -5,7 +5,7 @@ let redisClient_config = null;
 
 function createClient() {
     if (redisClient_config === null) {
-        redisClient_config = new Redis(process.env['REDIS_URL_ball']);
+        redisClient_config = new Redis(process.env['REDIS_URL_ball1']);
         redisClient_config.on('error', function (err) {
             console.error('redisClient error', err)
         });
